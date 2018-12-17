50de4d41cc9f24ceea1c3224266e19c46c17c625
Andrew Arpasi <andrew@andrewarpasi.com>
2018-12-16 22:22:49 -0500

    Remove clutter

--- keyboard.go
+++ keyboard.go
@@ -7,34 +7,6 @@ import (
 
 var mutex = &sync.Mutex{}
 
-func keytype() {
-	// robotgo.TypeString("Hello World")
-	// robotgo.TypeString("测试")
-	// robotgo.TypeStr("测试")
-	robotgo.Sleep(2)
-	robotgo.TypeStr("山达尔星新星军团, galaxy. こんにちは世界.")
-
-	// ustr := uint32(robotgo.CharCodeAt("测试", 0))
-	// robotgo.UnicodeType(ustr)
-
-	// robotgo.KeyTap("enter")
-	// robotgo.TypeString("en")
-	// robotgo.KeyTap("i", "alt", "command")
-	// arr := []string{"alt", "command"}
-	// robotgo.KeyTap("i", arr)
-
-	// robotgo.WriteAll("Test")
-	// text, err := robotgo.ReadAll()
-	// if err == nil {
-	// 	fmt.Println(text)
-	// }
-}
-
-func mouse() {
-	robotgo.Sleep(5)
-	robotgo.MoveMouseSmooth(100, 200, 1.0, 100.0)
-}
-
 func keyPress(key string, modifiers ...string) {
 	mutex.Lock()
 	robotgo.KeyToggle(key,"down")
